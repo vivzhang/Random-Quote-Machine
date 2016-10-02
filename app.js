7164d969917a158b11fe0249e422d6ba06f3b1c8
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-02 11:51:42 -0700

    added api, able to show one random quote every time user clicks the button

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+  var counter=1;
+  $("#quotebutton").on("click", function() {
+    $.ajax({
+      url: 'https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+      success: function(data) {
+        $('.quotemessage').html(data.quoteText + "<br> - " + data.quoteAuthor);
+      },
+      cache: false
+    })
+    console.log(counter)
+    $(".quotecount").html(counter);
+    counter++;
+    console.log(counter);
+  });
 })
